941feaf0c57f11f0bbfe35a88f21bc15824458dd
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2024-10-26 22:59:44 -0700

    Initial Commit

---
+++ CoffeNoteTests/CoffeNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeNoteTests.swift
+//  CoffeNoteTests
+//
+//  Created by Vatsal Chandel on 10/26/24.
+//
+
+import Testing
+@testable import CoffeNote
+
+struct CoffeNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
